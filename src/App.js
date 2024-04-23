231274162e587fb44da084262f4d8ed06b9a7fb3
Brian Yrgon <yegonkimutai.99@gmail.com>
2024-04-23 17:53:11 +0300

    :construction: Create customization page layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Customizer from "./pages/Customizer";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <Customizer />
     </div>
   );
 }
